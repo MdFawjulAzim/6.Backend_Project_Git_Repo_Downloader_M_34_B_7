b1f00174fda72f9ac5de96ca232eddbd64772853
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-04-09 00:16:10 +0600

    app.js file setup

---
+++ app/config/config.js
@@ -0,0 +1,17 @@
+export const MONGODB_CONNECTION="";
+
+
+export const JWT_SECRET = "GitREPODOWONLOAD123";
+export const JWT_EXPIRATION_TIME = 60*60*24*30;
+
+export const Max_JSON_SIZE = "50mb";
+export const URL_ENCODER = true;
+
+
+export const REQUEST_LIMIT_TIME = 15*60*1000;//15 min
+export const REQUEST_LIMIT_NUMBER =3000;//per 15 Min 3000 Requests Allowed
+
+
+export const WEB_CACHE=false;
+export const PORT=5050;
+
