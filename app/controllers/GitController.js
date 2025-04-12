2bd2c0f668c3c7dd18134aa6ea242d27011748b7
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-04-12 16:23:44 +0600

    done

---
+++ app/controllers/GitController.js
@@ -0,0 +1,46 @@
+import path from "path";
+import fs from "fs";
+import { exec } from "child_process";
+import util from "util";
+
+//Promisify exec
+const execPromise = util.promisify(exec);
+
+export const downloadRepo = async (req, res) => {
+  try {
+    const { owner, repo } = req.params;
+    //! GitHub repository URL
+    const repoUrl = `https://github.com/${owner}/${repo}.git`;
+
+    // Generate a unique folder name based on timestamp
+
+    const timestamp = Date.now();
+
+    const localFolder = path.join(
+      process.cwd(),
+      "myRepository",
+      `${repo}-${timestamp}`
+    );
+
+    // Ensure the folder exists or create it
+    if (!fs.existsSync(localFolder)) {
+      fs.mkdirSync(localFolder, { recursive: true });
+    }
+
+    //wrap the localFolder path in quotes to handle spaces
+    const cloneCommand = `git clone ${repoUrl} *${localFolder}*`;
+
+    //execute the clone command
+    console.log(`Executing:${cloneCommand}`);
+    await execPromise(cloneCommand);
+
+    res
+      .status(200)
+      .json({ message: `Repository cloned successfully to ${localFolder}` });
+  } catch (e) {
+    res.status(500).json({
+      message: "An error occurred while cloning the repository.",
+      error: error.message,
+    });
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,8 @@
-import express from 'express';
-const router =express.Router();
+import express from "express";
+const router = express.Router();
 
+import * as GitController from "../app/controllers/GitController.js";
 
-
-
-
-
+router.get("/:owner/:repo", GitController.downloadRepo);
 
 export default router;
